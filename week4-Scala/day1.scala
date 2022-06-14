c719753cc4b7944faead730ee104073740b603d9
william davis <williamjsdavis@berkeley.edu>
2022-06-14 15:12:02 -0700

    Scala day 1 continued

--- week4-Scala/day1.scala
+++ week4-Scala/day1.scala
@@ -35,6 +35,54 @@ object Main {
       }
     }
     for_loop
+
+    /* Ranges and tuples */
+    val a = 1 until 3
+    val b = (0 to 10) by 2
+    println(a)
+    println(b)
+
+    val elvis = ("Elvis", "Presley")
+    println(elvis)
+    println(elvis._1)
+
+    /* Classes */
+    val someone = new Person("Tom", "Simkin")
+    println(someone)
+
+    val mycompass = new Compass
+    mycompass.turnRight()
+    mycompass.turnRight()
+    mycompass.turnLeft()
+    mycompass.turnRight()
+
+    /* Extending classes: companion objects*/
+    Magnet.action
   }
+}
+class Person(firstName: String, lastName: String)
+class Compass {
+  val directions = List("north", "east", "south", "west")
+  var bearing = 0
+  print("Initial bearing: ")
+  println(direction)
+
+  def direction() = directions(bearing)
+
+  def inform(turnDirection: String) {
+    println("Turning " + turnDirection + ". Now bearing " + direction)
+  }
+
+  def turnRight() {
+    bearing = (bearing + 1) % directions.size
+    inform("right")
+  }
+  def turnLeft() {
+    bearing = (bearing + (directions.size - 1)) % directions.size
+    inform("left")
+  }
+}
+object Magnet {
+  def action = println("Point North")
 }
 
